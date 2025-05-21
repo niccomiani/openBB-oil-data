773347c2a28980cd118f66a0d4d3bc8786cdcbee
niccomiani <miani.niccolo01@gmail.com>
2025-05-21 12:49:27 +0200

    wti_update.R

---
+++ wti_update.R
@@ -0,0 +1,12 @@
+wti_update.R
+
+library(quantmod)
+
+symbol <- "CL=F"
+getSymbols(symbol, src = "yahoo", from = "2024-01-01", auto.assign = TRUE)
+
+wti <- Cl(get(symbol))
+wti_df <- data.frame(Date = index(wti), Close = coredata(wti))
+
+write.csv(wti_df, "wti_prices.csv", row.names = FALSE)
+
